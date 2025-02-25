4a8448751834ebaa12d6d7210039635d276c8969
JeffRisberg <j.s.risberg@gmail.com>
2025-02-24 23:55:09 -0800

    added additional tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
 -- Drop existing tables in reverse order of dependencies
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS organizations;
+DROP TABLE IF EXISTS interactions;
 DROP TABLE IF EXISTS opportunities;
 DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS relationships;
@@ -6,7 +9,8 @@ DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS tenants;
 
 -- Create tables
-CREATE TABLE tenants (
+CREATE TABLE tenants
+(
     id          BIGINT(10) NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255)       DEFAULT null,
     email       VARCHAR(255)       DEFAULT null,
@@ -14,91 +18,73 @@ CREATE TABLE tenants (
     logo_url    TEXT               DEFAULT NULL,
     status      ENUM('Active','Deleted') NOT NULL DEFAULT 'Active',
     created_at  TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE accounts (
+CREATE TABLE accounts
+(
     id                       BIGINT(10) NOT NULL AUTO_INCREMENT,
     tenant_id                BIGINT(10) NOT NULL,
-    phone_number             VARCHAR(255)       DEFAULT null,
+    phone_number             VARCHAR(255)          DEFAULT null,
     role                     VARCHAR(255) NOT NULL DEFAULT 'Student', -- Handle at source code level (Student, Adult, Expert, SystemAdmin)
-    salutation               VARCHAR(255)       DEFAULT null,
-    first_name               VARCHAR(255)       DEFAULT null,
-    last_name                VARCHAR(255)       DEFAULT null,
-    email_address            VARCHAR(255)       DEFAULT null,
-    job_name                 VARCHAR(255)       DEFAULT null,
-    skills                   TEXT               DEFAULT "",
-    field                    TEXT               DEFAULT "",
-    internship_experience    BIT(1)             DEFAULT 0,
-    soc_cap_index            INT                DEFAULT 0,
-    assistant_id             VARCHAR(255)       DEFAULT null,
-    thread_id                VARCHAR(255)       DEFAULT null,
+    salutation               VARCHAR(255)          DEFAULT null,
+    first_name               VARCHAR(255)          DEFAULT null,
+    last_name                VARCHAR(255)          DEFAULT null,
+    email_address            VARCHAR(255)          DEFAULT null,
+    job_name                 VARCHAR(255)          DEFAULT null,
+    skills                   TEXT                  DEFAULT "",
+    field                    TEXT                  DEFAULT "",
+    internship_experience    BIT(1)                DEFAULT 0,
+    soc_cap_index            INT                   DEFAULT 0,
+    assistant_id             VARCHAR(255)          DEFAULT null,
+    thread_id                VARCHAR(255)          DEFAULT null,
     status                   ENUM('Active','Deleted') NOT NULL DEFAULT 'Active',
-    motivation               INT                DEFAULT 0,
-    stress                   INT                DEFAULT 0,
-    help_seeking             BIT(1)             DEFAULT 0,
-    resource_awareness       BIT(1)             DEFAULT 0,
-    education_attainment     BIT(1)             DEFAULT 0,
-    relevant_work_experience BIT(1)             DEFAULT 0,
-    life_sustaining_career   BIT(1)             DEFAULT 0,
-    life_sustaining_wage     BIT(1)             DEFAULT 0,
-    timezone                 VARCHAR(255)       DEFAULT 'America/Los_Angeles',
-    created_at               TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    updated_at               TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    motivation               INT                   DEFAULT 0,
+    stress                   INT                   DEFAULT 0,
+    help_seeking             BIT(1)                DEFAULT 0,
+    resource_awareness       BIT(1)                DEFAULT 0,
+    education_attainment     BIT(1)                DEFAULT 0,
+    relevant_work_experience BIT(1)                DEFAULT 0,
+    life_sustaining_career   BIT(1)                DEFAULT 0,
+    life_sustaining_wage     BIT(1)                DEFAULT 0,
+    timezone                 VARCHAR(255)          DEFAULT 'America/Los_Angeles',
+    created_at               TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
+    updated_at               TIMESTAMP    NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
     PRIMARY KEY (id),
-    FOREIGN KEY (tenant_id)  REFERENCES tenants(id)
+    FOREIGN KEY (tenant_id) REFERENCES tenants (id)
 );
 
-CREATE TABLE relationships (
-    id                       BIGINT(10) NOT NULL AUTO_INCREMENT,
-    from_account_id          BIGINT(10) NOT NULL,
-    to_account_id            BIGINT(10) NOT NULL,
-    type                     ENUM('Peer','Advisor','Parent') NOT NULL DEFAULT 'Advisor',
-    interaction_count        INT DEFAULT 0,
-    last_interaction_date    TIMESTAMP DEFAULT NULL,
-    led_to_opportunity       BIT(1) DEFAULT 0,
-    created_at               TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    updated_at               TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+CREATE TABLE relationships
+(
+    id                    BIGINT(10) NOT NULL AUTO_INCREMENT,
+    from_account_id       BIGINT(10) NOT NULL,
+    to_account_id         BIGINT(10) NOT NULL,
+    type                  ENUM('Peer','Advisor','Parent') NOT NULL DEFAULT 'Advisor',
+    interaction_count     INT                DEFAULT 0,
+    last_interaction_date TIMESTAMP          DEFAULT NULL,
+    led_to_opportunity    BIT(1)             DEFAULT 0,
+    created_at            TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    updated_at            TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
     PRIMARY KEY (id),
-    FOREIGN KEY (from_account_id) REFERENCES accounts(id),
-    FOREIGN KEY (to_account_id) REFERENCES accounts(id)
+    FOREIGN KEY (from_account_id) REFERENCES accounts (id),
+    FOREIGN KEY (to_account_id) REFERENCES accounts (id)
 );
 
-CREATE TABLE messages (
-    id                       BIGINT(10) NOT NULL AUTO_INCREMENT,
-    account_id               BIGINT(10) NOT NULL,
-    role                     VARCHAR(255) DEFAULT NULL,
-    text                     TEXT DEFAULT NULL,
-    flagged                  BIT(1) DEFAULT 0,
-    flagged_reason           VARCHAR(255) DEFAULT NULL,
-    created_at               TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    updated_at               TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+CREATE TABLE messages
+(
+    id             BIGINT(10) NOT NULL AUTO_INCREMENT,
+    account_id     BIGINT(10) NOT NULL,
+    role           VARCHAR(255)       DEFAULT NULL,
+    text           TEXT               DEFAULT NULL,
+    flagged        BIT(1)             DEFAULT 0,
+    flagged_reason VARCHAR(255)       DEFAULT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    updated_at     TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
     PRIMARY KEY (id),
-    FOREIGN KEY (account_id) REFERENCES accounts(id)
+    FOREIGN KEY (account_id) REFERENCES accounts (id)
 );
 
-CREATE TABLE opportunities (
-    id                        BIGINT(10) NOT NULL AUTO_INCREMENT,
-    source_account_id         BIGINT(10) NOT NULL,
-    recipient_account_id      BIGINT(10) NOT NULL,
-    relationship_id           BIGINT(10) DEFAULT NULL,
-    opportunity_type          ENUM('Internship','Job','Mentorship','Project') NOT NULL,
-    title                     VARCHAR(255) DEFAULT NULL,
-    description               TEXT DEFAULT NULL,
-    status                    ENUM('Open','Applied','Interview','Accepted','Rejected','Expired') NOT NULL DEFAULT 'Open',
-    source_type               ENUM('NetworkConnection','JobBoard','DirectApplication','Other') NOT NULL,
-    compensation_type         ENUM('Paid','Unpaid','Unknown') DEFAULT 'Unknown',
-    application_date          TIMESTAMP DEFAULT NULL,
-    outcome_date              TIMESTAMP DEFAULT NULL,
-    notes                     TEXT DEFAULT NULL,
-    created_at                TIMESTAMP NOT NULL DEFAULT current_timestamp(),
-    updated_at                TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    PRIMARY KEY (id),
-    FOREIGN KEY (source_account_id) REFERENCES accounts(id),
-    FOREIGN KEY (recipient_account_id) REFERENCES accounts(id),
-    FOREIGN KEY (relationship_id) REFERENCES relationships(id)
-);
 
 INSERT INTO tenants(name, email, status, logo_url)
 VALUES ('Making Waves', 'admin@making_waves.org', 'Active', 'http://making_waves.com/logo');
@@ -179,3 +165,70 @@ INSERT INTO relationships(from_account_id, to_account_id, type)
 VALUES (4, 6, 'Advisor');
 INSERT INTO relationships(from_account_id, to_account_id, type)
 VALUES (5, 7, 'Advisor');
+
+
+CREATE TABLE opportunities
+(
+    id                   BIGINT(10) NOT NULL AUTO_INCREMENT,
+    source_account_id    BIGINT(10) NOT NULL,
+    recipient_account_id BIGINT(10) NOT NULL,
+    relationship_id      BIGINT(10) DEFAULT NULL,
+    opportunity_type     ENUM('Internship','Job','Mentorship','Project') NOT NULL,
+    title                VARCHAR(255)       DEFAULT NULL,
+    description          TEXT               DEFAULT NULL,
+    status               ENUM('Open','Applied','Interview','Accepted','Rejected','Expired') NOT NULL DEFAULT 'Open',
+    source_type          ENUM('NetworkConnection','JobBoard','DirectApplication','Other') NOT NULL,
+    compensation_type    ENUM('Paid','Unpaid','Unknown') DEFAULT 'Unknown',
+    application_date     TIMESTAMP          DEFAULT NULL,
+    outcome_date         TIMESTAMP          DEFAULT NULL,
+    notes                TEXT               DEFAULT NULL,
+    created_at           TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    updated_at           TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
+    PRIMARY KEY (id),
+    FOREIGN KEY (source_account_id) REFERENCES accounts (id),
+    FOREIGN KEY (recipient_account_id) REFERENCES accounts (id),
+    FOREIGN KEY (relationship_id) REFERENCES relationships (id)
+);
+
+CREATE TABLE interactions
+(
+    id              BIGINT(10) NOT NULL AUTO_INCREMENT,
+    relationship_id BIGINT(10) DEFAULT NULL,
+    type            ENUM('Email','Text','Online', 'In-Person') NOT NULL DEFAULT 'Email',
+    date            TIMESTAMP          DEFAULT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+    updated_at      TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
+    PRIMARY KEY (id),
+    FOREIGN KEY (relationship_id) REFERENCES relationships (id)
+);
+
+CREATE TABLE organizations
+(
+    id         BIGINT(10) NOT NULL AUTO_INCREMENT,
+    name       varchar(255) NOT NULL,
+    importance BIGINT(10) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
+    updated_at TIMESTAMP    NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE events
+(
+    id              BIGINT(10) NOT NULL AUTO_INCREMENT,
+    organization_id BIGINT(10) DEFAULT NULL,
+    name            varchar(255) NOT NULL,
+    description     varchar(255)          DEFAULT "",
+    created_at      TIMESTAMP    NOT NULL DEFAULT current_timestamp(),
+    updated_at      TIMESTAMP    NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp (),
+    PRIMARY KEY (id),
+    FOREIGN KEY (organization_id) REFERENCES organizations (id)
+);
+
+INSERT INTO organizations(name, importance)
+VALUES ('MakingWaves Students', 10);
+INSERT INTO organizations(name, importance)
+VALUES ('MakingWaves Academy', 9);
+INSERT INTO organizations(name, importance)
+VALUES ('MakingWaves Educational Foundation', 9);
+INSERT INTO organizations(name, importance)
+VALUES ('MakingWaves Alumni Organization', 8);
